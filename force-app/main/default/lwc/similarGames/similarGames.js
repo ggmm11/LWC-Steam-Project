1ec3ada87ed18299bcf0e7f8c7a1c547f4fe4fb6
A Y <yad5861839@hotmail.com>
2021-08-24 21:58:45 +0100

    Added similar car component

--- force-app/main/default/lwc/similarGames/similarGames.js
+++ force-app/main/default/lwc/similarGames/similarGames.js
@@ -1,3 +1,42 @@
-import { LightningElement } from 'lwc';
+import { LightningElement,api,wire } from 'lwc';
+import { getRecord } from 'lightning/uiRecordApi';
+import GAME_TAB_FIELD from '@salesforce/schema/Game__c.Game_Tab__c'
+import getSimilarGames from '@salesforce/apex/GameController.getSimilarGames'
 
-export default class SimilarGames extends LightningElement {}
+import {NavigationMixin} from 'lightning/navigation'
+export default class SimilarGames extends NavigationMixin(LightningElement) {
+    similarGames
+
+    @api recordId;
+    @api objectApiName;
+
+    @wire(getRecord,{recordId: '$recordId', fields:[GAME_TAB_FIELD]})
+    game
+
+    //fetch the simliar game by the Apex class
+    fetchSimilarGames(){
+        getSimilarGames({
+            gameId:this.recordId, //pass the Id to Apex as paramter
+            gameTab:this.game.data.fields.Game_Tab__c.value // pass the game tab value to Apex as parameter
+        }).then(result=>{
+            this.similarGames = result //assign the value into similarGames
+            console.log(this.similarGames)
+        }).catch(error=>{
+            console.error(error)
+        })
+    }
+
+    //Access to the other game that similar to the current games
+    handleViewDetailsClick(event){
+        this[NavigationMixin.Navigate]({
+            type:'standard__recordPage',
+            attributes:{
+                recordId:event.target.dataset.id,
+                objectApiName: this.objectApiName,
+                actionName:'view'
+            }
+        })
+    }
+
+    
+}
